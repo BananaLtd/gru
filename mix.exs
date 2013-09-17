27815b4648a0efd32d57cc4cd1218d7882100328
Steffen Schröder <steffen@schroeder-blog.de>
2013-09-17 08:41:01 +0200

    upgrade minion to version 0.0.2

--- mix.exs
+++ mix.exs
@@ -19,6 +19,6 @@ defmodule Gru.Mixfile do
   # Returns the list of dependencies in the format:
   # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
-    [ { :minion, github: "BananaLtd/minion" } ]
+    [ { :minion, "0.0.2", github: "BananaLtd/minion" } ]
   end
 end
